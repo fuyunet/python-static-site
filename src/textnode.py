3b0eccc7837b2628bd27e60b182a2d7a0bf88dd6
fuyunet <fuyune.horii@gmail.com>
2024-03-18 08:51:15 +0900

    Fixed few things

--- src/textnode.py
+++ src/textnode.py
@@ -3,9 +3,13 @@ class TextNode:
         self.text = text
         self.text_type = text_type
         self.url = url
-    
-    def __eq__(self, textnode: 'TextNode') -> bool:
-        return self.text == textnode.text and self.text_type == textnode.text_type and self.url == textnode.url
-    
+
+    def __eq__(self, textnode: "TextNode") -> bool:
+        return (
+            self.text == textnode.text
+            and self.text_type == textnode.text_type
+            and self.url == textnode.url
+        )
+
     def __repr__(self) -> str:
-        return "TextNode(" + self.text + ", " + self.text_type + ", " + self.url + ")"
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
