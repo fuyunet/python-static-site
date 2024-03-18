653e365c58195c981de82a199e998851aed652cb
fuyunet <fuyune.horii@gmail.com>
2024-03-18 19:45:18 +0900

    added LeafNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,8 +24,24 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return (
+            "HTMLNode: ("
             f"tag: {self.tag}, "
             f"value: {self.value}, "
             f"children: {self.children}, "
-            f"props: {self.props}"
+            f"props: {self.props})"
         )
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: dict = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -10,6 +10,22 @@ class TestHTMLNode(unittest.TestCase):
         )
         actual = props_node.props_to_html()
         expected = ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(actual, expected)
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_eq_leaf_node_to_html_with_props(self):
+        leaf_note = LeafNode(
+            "a", "Click me!", {"href": "https://www.google.com"}
+        )
+        actual = leaf_note.to_html()
+        expected = '<a href="https://www.google.com">Click me!</a>'
         self.assertEqual(actual, expected)
 
 
