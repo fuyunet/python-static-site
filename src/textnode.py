e12a7587296d7238ac3d59b41cf5aa315b2ef6d7
fuyunet <fuyune.horii@gmail.com>
2024-03-21 00:17:36 +0900

    Chapter 3, split delimiter

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 text_type_text = "text"
 text_type_bold = "bold"
 text_type_italic = "italic"
@@ -21,3 +23,35 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node: "TextNode"):
+    if text_node.text_type is text_type_text:
+        return LeafNode(value=text_node.text)
+
+    if text_node.text_type is text_type_bold:
+        return LeafNode(tag="b", value=text_node.text)
+
+    if text_node.text_type is text_type_italic:
+        return LeafNode(tag="i", value=text_node.text)
+
+    if text_node.text_type is text_type_code:
+        return LeafNode(tag="code", value=text_node.text)
+
+    if text_node.text_type is text_type_link:
+        return LeafNode(
+            tag="a", value=text_node.text, props={"href": text_node.url}
+        )
+
+    if text_node.text_type is text_type_image:
+        return LeafNode(
+            tag="img",
+            value="",
+            props={"src": text_node.url, "alt": text_node.text},
+        )
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
+
+
+# TODO: Chapter 3: Inline, 1.Split delimiter
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    pass
