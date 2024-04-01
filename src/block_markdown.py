36875ef7405743f8c02d6473e42f946df97e1d1e
fuyunet <fuyune.horii@gmail.com>
2024-04-02 08:47:42 +0900

    Chapter 4.2, added block_to_block_type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,11 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+
 def markdown_to_blocks(markdwon: str) -> list[str]:
     block_str = markdwon.split("\n\n")
     filtered_blocks = []
@@ -7,3 +15,38 @@ def markdown_to_blocks(markdwon: str) -> list[str]:
         str = str.strip()
         filtered_blocks.append(str)
     return filtered_blocks
+
+
+def block_to_block_type(block: str) -> str:
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+
+    lines = block.split("\n")
+
+    if all(line.startswith(">") for line in lines):
+        return block_type_quote
+    if all(line.startswith(("*", "-")) for line in lines):
+        return block_type_unordered_list
+
+    tmp_bool = True
+    cur_num = 1
+    for line in lines:
+        if line.startswith(str(cur_num) + "."):
+            cur_num += 1
+            continue
+        tmp_bool = False
+        break
+    if tmp_bool:
+        return block_type_ordered_list
+    return block_type_paragraph
+
+    pass
